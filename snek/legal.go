90d84c87e75a9bc449af35435b5bcf053a1e9fed
Amy <amyadzuki@gmail.com>
2018-08-11 10:24:45 +0000

    Update legal.go

--- snek/legal.go
+++ snek/legal.go
@@ -10,20 +10,28 @@ import (
 var (
 	// The full legal text of the End User License Agreement (EULA)
 	Eula string
+
+	// The short identifier for the license as used on ChooseALicense.com
+	License string = "Proprietary"
 )
 
 type Legal struct {
 	Copyright Copyright `json:"copyright" xml:"copyright,attr"`
 	Eula string `json:"eula" xml:"eula,attr"`
+	License string `json:"license" xml:"license,attr"`
 }
 
-func NewLegal(copyright *Copyright, eula string) *Legal {
-	return new(Legal).Init(copyright, eula)
+func NewLegal(copyright *Copyright, license, eula string) *Legal {
+	return new(Legal).Init(copyright, license, eula)
 }
 
-func (legal *Legal) Init(copyright *Copyright, eula string) *Legal {
+func (legal *Legal) Init(copyright *Copyright, license, eula string) *Legal {
 	legal.Copyright = *copyright
 	legal.Eula = eula
+	legal.License = license
+	if strings.Contains(license, "\n") {
+		panic("License should be the short identifier.  Use EULA for the full text.")
+	}
 }
 
 func (legal *Legal) Json(onFail ...onfail.OnFail) string {
